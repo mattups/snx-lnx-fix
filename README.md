9fa5164d1af0126ad92fbac253c49a512f67d32c
Mattia Tupone <tupone.mattia@gmail.com>
2019-03-06 21:33:25 +0100

    Updated script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # snx-lnx-fix
 A fix for Checkpoint's SNX linux VPN client.
+
+Just `chmod +x snx-fix.sh` and `./snx-fix.sh` to install the SNX client from Checkpoint.
--- snx-fix.sh
+++ snx-fix.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-# Add i386 architecture. Needed for some packages.
+# Adding i386 architecture. Needed for some packages.
 sudo dpkg --add-architecture i386 
 sudo apt-get update
-sudo apt-get install libc6:i386
+sudo apt-get install libc6-i386 libaudit1 libxcb1 libgcc1 libc6 lib32tinfo5  libaudit1 -y
+sudo apt -f install -y
 
 # Install debs for snx.
 cd ./debs
 sudo dpkg -i ./*
+sudo apt -f install -y
 
-# Install snx
-cd ./snx
+# Installing snx.
+cd ../snx
+mkdir ./installer
 
-tar -xvzf /snx/SNX_for_VPN1_Win7.tgz ./installer
-sudo chmod +x ./installer/snx_install.sh
+tar -xvzf SNX_for_VPN1_Win7.tgz 
+sudo chmod +x ./snx_install.sh
 
-cd ./installer && sudo ./snx_install.sh
+sudo ./snx_install.sh
+
+snx
