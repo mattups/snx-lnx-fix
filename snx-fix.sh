15564b7ccbdac5bf9f9fcccf67628b63fcaf7e03
Mattia Tupone <tupone.mattia@gmail.com>
2019-03-07 21:12:17 +0100

    Updated debs, main script.

--- snx-fix.sh
+++ snx-fix.sh
@@ -2,21 +2,15 @@
 # Adding i386 architecture. Needed for some packages.
 sudo dpkg --add-architecture i386 
 sudo apt-get update
-sudo apt-get install libc6-i386 libaudit1 libxcb1 libgcc1 libc6 lib32tinfo5  libaudit1 -y
+sudo apt-get install libc6-i386 libaudit1 libxcb1 libgcc1 libc6 lib32tinfo5 libaudit1 -y
 sudo apt -f install -y
 
 # Install debs for snx.
-cd ./debs
-sudo dpkg -i ./*
+sudo dpkg -i ./debs/*.deb
 sudo apt -f install -y
 
 # Installing snx.
-cd ../snx
-mkdir ./installer
-
-tar -xvzf SNX_for_VPN1_Win7.tgz 
-sudo chmod +x ./snx_install.sh
-
-sudo ./snx_install.sh
+sudo chmod +x ./snx_install_linux30_7075.sh
+sudo ./snx-install-7075.sh
 
 snx
