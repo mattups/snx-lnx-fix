9d672a08cb1d2e6581f8dc1e4312bf6016072c36
Simone Di Cicco <simone.dicicco@gmail.com>
2019-03-07 19:18:33 +0100

    Added correct apt-get package list + added SNX Build 7075 installer

--- snx-fix.sh
+++ snx-fix.sh
@@ -2,21 +2,13 @@
 # Adding i386 architecture. Needed for some packages.
 sudo dpkg --add-architecture i386 
 sudo apt-get update
-sudo apt-get install libc6-i386 libaudit1 libxcb1 libgcc1 libc6 lib32tinfo5  libaudit1 -y
-sudo apt -f install -y
-
-# Install debs for snx.
-cd ./debs
-sudo dpkg -i ./*
+sudo apt-get install libc6-i386 libaudit1 libxcb1 libgcc1 libc6 lib32tinfo5 libaudit1 libx11-6:i386 libstdc++5:i386 libpam0g:i386 -y
 sudo apt -f install -y
 
 # Installing snx.
 cd ../snx
-mkdir ./installer
+sudo chmod +x ./snx_install_linux30
 
-tar -xvzf SNX_for_VPN1_Win7.tgz 
-sudo chmod +x ./snx_install.sh
-
-sudo ./snx_install.sh
+sudo ./snx_install_linux30
 
 snx
