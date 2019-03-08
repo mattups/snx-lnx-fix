eefe826267547d4fe0c03bae270c0aba8c6d8624
Mattia Tupone <tupone.mattia@gmail.com>
2019-03-08 18:24:21 +0100

    Updated main script. A snx config file is created now

--- snx-fix.sh
+++ snx-fix.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Adding Oracle Java.
-# sudo add-apt-repository -y ppa:webupd8team/java
-# echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-# sudo apt-get install oracle-java8-installer libstdc++5:i386 libpam0g:i386 libx11-6:i386 -y
-
 # Adding i386 architecture. Needed for some packages.
 sudo dpkg --add-architecture i386 
 sudo apt-get update
@@ -12,8 +7,18 @@ sudo apt-get install libc6-i386 libaudit1 libxcb1 libgcc1 libc6 lib32tinfo5 liba
 sudo apt -f install -y
 
 # Installing snx.
-cd ../snx
-sudo chmod +x ./snx_install_linux30
+sudo chmod +x ./snx-install-7075.sh
+sudo ./snx-install-7075.sh
 
-sudo ./snx_install_linux30
+# Creating config file for snx.
+printf "\n"
+read -p "Your VPN server: " vpnserver
+read -p "Your username: " username
+printf "\nPlease insert your VPN password below.\n\n"
+
+echo "server $vpnserver
+username $username
+reauth yes" > ~/.snxrc
+
+# Connection.
 snx
